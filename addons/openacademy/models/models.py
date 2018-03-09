ab143673d4159e43c9f5ef2a78a41fef7a4d35c9
cjmeilan <info@gmagestion.com>
2018-03-09 15:00:51 -0300

    res

--- addons/openacademy/models/models.py
+++ addons/openacademy/models/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api, exceptions
+from odoo.exceptions import ValidationError
 
 class Course(models.Model):
     _name = 'openacademy.course'
@@ -12,6 +13,17 @@ class Course(models.Model):
    
     session_ids = fields.One2many(
         'openacademy.session', 'course_id', string="Sessions")
+
+    _sql_constraints = [
+        ('name_description_check',
+         'CHECK(name != description)',
+         "The title of the course should not be the description"),
+
+        ('name_unique',
+         'UNIQUE(name)',
+         "The course title must be unique"),
+    ]
+
 
 
 
@@ -29,7 +41,7 @@ class Session(models.Model):
 		('category_id.name', 'ilike', "Teacher")])
 	course_id = fields.Many2one('openacademy.course', ondelete='cascade', string='Course', required='True')
 	attendee_ids = fields.Many2many('res.partner', string="Attendees")
-
+	
 	taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
 
 	@api.depends('seats', 'attendee_ids')
@@ -62,6 +74,22 @@ class Session(models.Model):
 			if r.instructor_id and r.instructor_id in r.attendee_ids:
 				raise exceptions.ValidationError("A session's instructor can't be an attendee")
 
+	@api.onchange('seats', 'attendee_ids')
+	def _verify_valid_seats(self):
+		if self.seats < 0:
+			return {
+    		'warning': {
+    				'title': "Incorrect 'seats' value",
+                    'message': "The number of available seats may not be negative",
+                },
+            }
+		if self.seats < len(self.attendee_ids):
+			return {
+		                'warning': {
+		                    'title': "Too many attendees",
+		                    'message': "Increase seats or remove excess attendees",
+		                },
+		            }
 
 	    
 
